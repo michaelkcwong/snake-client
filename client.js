1a39f8ca1ad4ff02777f67b63b41153b39b612b2
vagrant <wong_michael@rogers.com>
2021-11-18 04:16:06 +0000

    Added connect and name

--- client.js
+++ client.js
@@ -7,6 +7,14 @@ const connect = function() {
     port: 50541
   });
 
+  //code that does something when the connection is first established
+  conn.on("connect", () => {
+    console.log('Successfully connected to the game server') //Prints
+    conn.write("Name: M.W"); //Sends name to server upon connection
+  });
+  
+
+  //timer for being idle and prints below
   conn.setTimeout(3000);
   conn.on('timeout', () => {
     console.log('you ded cuz you idled');
