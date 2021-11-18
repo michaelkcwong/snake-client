494256207976ce4af8fba87c2bc9b77cef0dec73
vagrant <wong_michael@rogers.com>
2021-11-18 03:07:48 +0000

    Started play.js

---
+++ play.js
@@ -0,0 +1,26 @@
+const { constants } = require("buffer");
+const { Socket } = require("dgram");
+const net = require("net");
+
+//establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.setTimeout(3000);
+  conn.on('timeout', () => {
+    console.log('you ded cuz you idled');
+    conn.end();
+  });
+
+  //interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
