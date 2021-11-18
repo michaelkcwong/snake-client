8dc3fbcb1763662bce423c046c4771c4157458c8
vagrant <wong_michael@rogers.com>
2021-11-18 03:56:43 +0000

    Exported and imported play.js and client.js

--- play.js
+++ play.js
@@ -1,24 +1,28 @@
-const net = require("net");
+const { connect } = require("./client");
+// const net = require("net");
 
-//establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
+// //establishes a connection with the game server
+// const connect = function() {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541
+//   });
 
-  conn.setTimeout(3000);
-  conn.on('timeout', () => {
-    console.log('you ded cuz you idled');
-    conn.end();
-  });
+//   conn.setTimeout(3000);
+//   conn.on('timeout', () => {
+//     console.log('you ded cuz you idled');
+//     conn.end();
+//   });
 
-  //interpret incoming data as text
-  conn.setEncoding("utf8");
+//   //interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
 
+// module.exports = {
+//   connect,
+// }
