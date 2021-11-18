830b1f3e2751c054448432fcb895ac64b5ee291b
vagrant <wong_michael@rogers.com>
2021-11-18 06:55:39 +0000

    Added movement to snake

---
+++ input.js
@@ -0,0 +1,42 @@
+// Stores the active TCP connection object
+let connection;
+
+// setup interface to handle user input from stdin
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) { //check for the ctrl + c input and terminate the game
+  if (key === '\u0003') { // \u0003 is ctrl + c
+    process.exit();
+  }
+
+  if (key === "w") {
+    connection.write("Move: up"); // move up
+  }
+
+  if (key === "a") {
+    connection.write("Move: left"); // move left
+  }
+
+  if (key === "s") {
+    connection.write("Move: down"); // move down
+  }
+
+  if (key === "d") {
+    connection.write("Move: right"); //move right
+  }
+
+  console.log(key)
+};
+
+module.exports = {
+  setupInput,
+};
--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// setup interface to handle user input from stdin
+console.log("Connecting ...");
 
-const setupInput = function() {
-  connection = conn;
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-}
-
-const handleUserInput = function() { //check for the ctrl + c input and terminate the game
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-module.exports = {
-  setupInput,
-};
+let conn = connect();
+setupInput(conn);
